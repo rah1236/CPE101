9c4ddaf3af7462c3165eaadbd4346aa929fa5821
rah1236 <39041279+rah1236@users.noreply.github.com>
2022-04-19 17:29:02 -0700

    Finished Lab4

--- Lab4/Lab4_tests.py
+++ Lab4/Lab4_tests.py
@@ -7,16 +7,29 @@
 # Section:
 
 import unittest
-from functions import *
+import power4Table
+
 
 #You can dlete pass after wrinting your code
 class TestCases(unittest.TestCase):
    def test_factorial (self):
-      # Add code here.
-      pass
+      self.assertEqual(power4Table.factorial(5), 120)
+      self.assertEqual(power4Table.factorial(-8), 40320)
+      self.assertEqual(power4Table.factorial(2.6534), 2)
 
+
+   def test_income_tax (self):
+      self.assertEqual(power4Table.income_tax(5), 0.1)
+      self.assertEqual(power4Table.income_tax(10000), 0.12)
+      self.assertEqual(power4Table.income_tax(42000), 0.22)
+      self.assertEqual(power4Table.income_tax(90000), 0.24)
+      self.assertEqual(power4Table.income_tax(200000), 0.32)
+
+   def test_total_avg(self):
+        self.assertEqual(power4Table.total_avg(1, 500), 252.0)
+        self.assertEqual(power4Table.total_avg(-848, 40000), 19575.5)
+        self.assertEqual(power4Table.total_avg(111, 333), 220.5)
 
 # Run the unit tests.
 if __name__ == '__main__':
    unittest.main()
-
--- Lab4/power4Table.py
+++ Lab4/power4Table.py
@@ -1,12 +1,67 @@
 # CPE 101 Lab 4
-# Name:
-# Section:
+# Name:Raheel Rehmatullah
+# Section:7
+
+
+#Purpose: Read a string and see if it has the word "Accepted" in it
+# none -> none
+def read_str():
+    inputString = ""
+    while("accepted" not in inputString):
+        inputString = input("Please enter a sentence with the word 'accepted' in it.")
+    print("Cool, thank you!")
+
+#Purpose: Computes the factorial of given integer
+# int -> float
+def factorial(number):
+    newNumber = abs(int(number))
+    product = newNumber
+    for i in range(1, newNumber):
+        product *= i
+    return product
+
+#purpose: Takes income and computes income tax following the given table
+#int -> float
+def income_tax(income):
+    if (income >= 0 and income < 9950):
+        return(0.10)
+    elif(income >= 9951 and income < 40525):
+        return(0.12)
+    elif(income >= 40526 and income < 86375):
+        return(0.22)
+    elif(income >= 86376 and income < 164925):
+        return(0.24)
+    elif(income >= 164926 and income < 209425):
+        return(0.32)
+    else:
+        return(0)
+
+#purpose: This function must print the number that is divisible by 7 between the given interval and find the total and return the average of these numbers.
+#int int -> float
+def total_avg(num1, num2):
+    divisibleBySeven = []
+    sum = 0
+    for i in range(num1, num2):
+        if(i % 7 == 0):
+            divisibleBySeven.append(i)
+    if len(divisibleBySeven) > 0 :
+        for i in divisibleBySeven:
+            sum += i
+        return (sum/(len(divisibleBySeven)))
+    else:
+        return(0)
+
+
+
+
+
 
 def main():
    table_size = get_table_size()
+   increment = get_increment()
    while table_size != 0:
       first = get_first()
-      show_table(table_size, first)
+      show_table(table_size, first, increment)
       table_size = get_table_size()
 
 # Obtain a valid table size from the user
@@ -16,21 +71,48 @@ def get_table_size():
    while (size) < 0:
       print ("Size must be non-negative.")
       size = int(input("Enter number of rows in table (0 to end): "))
-      
+
    return size;
 
-# Obtain the first table entry from the user 
+# Obtain the first table entry from the user
 def get_first():
    first = int(input("Enter the value of the first number in the table: "))
+
+   while (first) < 0:
+      print ("First number must be non-negative.")
+      first = int(input("Enter the value of the first number in the table: "))
+
    return first;
 
+# Obtain the iterative table entry from the user
+def get_increment():
+   increment = int(input("Enter the increment between rows: "))
+
+   while (increment) < 0:
+      print ("Increment must be non-negative.")
+      increment = int(input("Enter the increment between rows: "))
+
+   return increment;
+
+
 # Display the table of power4
-def show_table(size, first):
+def show_table(size, first, increment):
    print ("A power4 table of size %d will appear here starting with %d." % (size, first))
    print ("Number  Power4")
-   
-   # Insert Loop Here
-   
+   sum = 0
+   for i in range(first, size * increment + first, increment):
+       text = ("{:<6} {:>6}").format(i, i**4)
+       print(text)
+       sum += (i**4)
+
+   print("The sum of power4 is: " + str(sum))
+
+
+
+
 
 if __name__ == "__main__":
    main()
+
+
+#show_table(4,2, 1)
